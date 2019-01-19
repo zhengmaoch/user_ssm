59afa831a9ee3e4452cc66b9a2176437ad07f950
zhengmaoch <zhengmaoch@qq.com>
2019-01-19 22:58:19 +0800

    添加mybatis逆向工程生成实体对象及mapper接口和实现xml

--- src/main/java/com/chang/domain/User.java
+++ src/main/java/com/chang/domain/User.java
@@ -1,16 +1,75 @@
 package com.chang.domain;
 
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
 public class User {
     private String id;
+
     private String username;
+
     private String password;
+
     private String email;
+
     private Date birthday;
+
     private String nikename;
-    private Date createdTime;
+
+    private Date createdtime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNikename() {
+        return nikename;
+    }
+
+    public void setNikename(String nikename) {
+        this.nikename = nikename == null ? null : nikename.trim();
+    }
+
+    public Date getCreatedtime() {
+        return createdtime;
+    }
+
+    public void setCreatedtime(Date createdtime) {
+        this.createdtime = createdtime;
+    }
 }
