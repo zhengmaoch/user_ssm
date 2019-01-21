146b4ccd1ef9f615b06ead3bbe1f1d903893ca1c
zhengmaoch <zhengmaoch@qq.com>
2019-01-21 17:16:57 +0800

    整合spring springMVC完成

---
+++ src/main/java/com/chang/dao/UserDao.java
@@ -0,0 +1,28 @@
+package com.chang.dao;
+
+import com.chang.domain.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    int getTotal();
+
+    int delete(String id);
+
+    int deleteAll();
+
+    int add(User record);
+
+    User getById(String id);
+
+    User getByUsernameAndPassword(String username, String password);
+
+    List<User> getByUsername(String username);
+
+    List<User> getAllUsers();
+
+    int update(User record);
+
+    List<User> list(int start, int count);
+}
---
+++ src/main/java/com/chang/domain/User.java
@@ -0,0 +1,22 @@
+package com.chang.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Date birthday;
+
+    private String nickname;
+
+    private Date createdtime;
+}
